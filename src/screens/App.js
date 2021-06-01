c29be9be91bb3ffbc2f81957e4f92b967fb1aac1
abraham-lawson <abraham.lawson.dev@gmail.com>
2021-06-02 01:47:51 +0700

    add splash screen

--- src/screens/App.js
+++ src/screens/App.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import {useSelector} from 'react-redux'
 import Navigation from 'navigation'
+import SplashScreen from 'react-native-splash-screen'
 
 const App = () => {
+	React.useEffect(() => SplashScreen.hide(), [])
 	const isLogged = useSelector((state) => state.Auth.isLogged)
 	return <Navigation isLogged={isLogged} />
 }
