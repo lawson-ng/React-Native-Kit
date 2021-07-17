ba2487eae17a219b55e40fded311e1167315492d
lawson <abraham.lawson.dev@gmail.com>
2021-07-18 00:05:17 +0700

    update eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,7 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
-  semi: ["error", "never"],
-};
+	root: true,
+	extends: '@react-native-community',
+	rules: {
+		semi: ['error', 'never'],
+	},
+}
